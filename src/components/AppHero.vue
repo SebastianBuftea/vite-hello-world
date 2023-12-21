0f137160a21a716e9f6f04b5e7c5f341041ac825
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-21 16:59:13 +0100

    [ADD] AppHero.vue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+
+}
+</script>
+<template lang="">
+    <div>
+        <img src="../assets/vue.svg" alt="">
+        <img src="/public/vite.svg" alt="">
+        <div class="container">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A doloribus 
+            nobis dignissimos in ipsa cupiditate repellendus voluptates quisquam velit eveniet? 
+            Repudiandae ipsa quia reprehenderit quo aliquid minima ex voluptatem vitae?</p>
+        </div>  
+    </div>
+</template>
+
+<style lang="">
+
+    
+</style>
